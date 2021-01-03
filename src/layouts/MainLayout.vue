393b833ee4bdea30d4e84d6c021835843a32f728
priaaji-seyanegoro <priaaji.setyanegoro@gmail.com>
2021-01-03 21:06:32 +0700

    init project, create home page , camera page, set routing, and create ui footer with tabs on layout ;

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer class="bg-white" bordered>
+      <q-tabs class="text-grey-10" active-color="primary">
+        <q-route-tab to="/" name="Home" icon="home" label="Home" />
+        <q-route-tab to="/camera" name="Camera" icon="camera" label="Camera" />
+      </q-tabs>
+    </q-footer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  data() {
+    return {};
+  }
+};
+</script>
