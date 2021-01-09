2e38f63b7985cf3260b5ad0474e33b7ee97233a1
priaaji-seyanegoro <priaaji.setyanegoro@gmail.com>
2021-01-09 08:21:25 +0700

    styling header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,14 +1,35 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title>
-          Quasar App
+    <q-header class="bg-white text-grey-10" bordered>
+      <q-toolbar class="constrain">
+        <q-btn
+          class="large-screen-only q-mr-sm"
+          flat
+          round
+          icon="eva-camera-outline"
+          dense
+          size="18px"
+          to="/camera"
+        />
+
+        <q-separator class="large-screen-only" vertical spaced />
+
+        <q-toolbar-title class="text-instagram text-bold">
+          Quasargram
         </q-toolbar-title>
+        <q-btn
+          class="large-screen-only"
+          flat
+          round
+          icon="eva-home-outline"
+          dense
+          size="18px"
+          to="/"
+        />
       </q-toolbar>
     </q-header>
 
-    <q-footer class="bg-white" bordered>
+    <q-footer class="bg-white small-screen-only" bordered>
       <q-tabs class="text-grey-10" active-color="primary">
         <q-route-tab to="/" icon="eva-home-outline" />
         <q-route-tab to="/camera" icon="eva-camera-outline" />
@@ -25,12 +46,19 @@ export default {
   name: "MainLayout",
   data() {
     return {};
-  }
+  },
 };
 </script>
 
 <style lang="sass">
+.q-toolbar
+  @media (min-width: $breakpoint-sm-min)
+    height: 67px
+.q-toolbar__title
+  font-size: 28px
+  @media (max-width: $breakpoint-xs-max)
+    text-align: center
 .q-footer
   .q-tab__icon
-    font-size: 30px
+    font-size: 28px
 </style>
